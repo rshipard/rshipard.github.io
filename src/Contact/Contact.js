408800a5984316ca6a3a90e9c196fd632d97584a
Ralph <rshipard@gmail.com>
2021-09-11 17:36:03 +1000

    placeholders 81% complete

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -7,9 +7,15 @@ class Contact extends Component {
             <div className="contact-me">
                 <h3>Contact Me on:</h3>
                 <ul className="hobbies-list">
-                    <li>Email</li>
-                    <li>LinkedIn</li>
-                    <li>Phone</li>
+                    <li>Connect with me on 
+                        <a href="https://www.linkedin.com/in/ralph-shipard-b58924200/" target="_blank"> LindedIn</a>
+                    </li>
+                    <li>Phone: 
+                        <a href="tel:0423626618"> 0423 626 618</a>
+                    </li>
+                    <li>Email: 
+                        <a href="mailto:rshipard@gmail.com"> rshipard@gmail.com</a>
+                    </li>
                 </ul>
             </div>
         )
