cdb4c0bde2750dcbd63b05c09bbc101fc3d94ff1
Ralph <rshipard@gmail.com>
2021-10-04 20:52:19 +1100

    contact typos

--- src/Views/Final/Contact/Contact.js
+++ src/Views/Final/Contact/Contact.js
@@ -6,7 +6,7 @@ function Contact(props) {
             <h3>Contact Me on:</h3>
             <ul className="hobbies-list no-bullets">
                 <li>Connect with me on 
-                    <a href="https://www.linkedin.com/in/ralph-shipard-b58924200/" target="_blank"> LindedIn</a>
+                    <a href="https://www.linkedin.com/in/ralph-shipard-b58924200/" target="_blank"> LinkedIn</a>
                 </li>
                 <li>Phone: 
                     <a href="tel:0423626618"> 0423 626 618</a>
