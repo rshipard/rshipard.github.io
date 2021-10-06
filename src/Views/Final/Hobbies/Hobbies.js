6874f1e38aac1d26fde08b2de0170b1042fcbc6d
Ralph <rshipard@gmail.com>
2021-10-06 18:12:37 +1100

    css fixes inc about me spacing

--- src/Views/Final/Hobbies/Hobbies.js
+++ src/Views/Final/Hobbies/Hobbies.js
@@ -7,31 +7,34 @@ function Hobbies(props) {
 
     return (
         <div className="HobbiesFinal">
-            <h3>Some of my hobbies include</h3>
-            <div className="hobby">
-                <h4>Theatre</h4>
-                <img className="float-left" src={theatrePoster} alt="" />
-                <p>Ever since Highschool I've loved theatre.</p>
-                <p>I have been able to have some of the best experiences of my life both on stage and off. I've acted, rigged lighting, created props and directed. All of which are completely different and completely rewarding.</p>
-                <p>Some of my favourite memories are being able to play as a villian with Castlemaine Theate Company which was a long term goal and a truly dark horror with Frankensteins Children.</p>
-                <p>Or the time I was asked to be the Technical Director for Bendigo Theatrre Company's TenX10 series of plays. Which was such a great learning experience and gave me new ways to stretch my skills in a really supportive environment.</p>
-            </div>
-            <div className="hobby">
-                <h4>Board Games</h4>
-                <img className="float-right boardgame-photo" src={boardGamePhoto} alt="" />
-                <p>Ever since I was really young I played games. First Checker with my grandfather and chess with my Dad.</p>
-                <p>When I was in my mid twenties I discovered so called "hobby" boardgames which turned first into a shelf, then a whole bookshelf, then a gaming club, and now the "shame cupboard".</p>
-                <p>We don't speak about the shame cupboard.</p>
-            </div>
-            <div className="hobby">
-                <h4>Dance</h4>
-                <p>In the last 6 years I've discovered the world of Ballroom and Latin dance. Due to little things like a pandemic I haven't been able to do it for the last 2 years. I can't wait for the post COVID world where I get to go back out and move across the dancefloor!</p>
-                <figure>
-                    <iframe className="video" width="560" height="315" src="https://www.youtube.com/embed/BFaA8Mm1syk?start=18" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
-                    <figcaption>Disclaimer - this is <i>not</i> me, but is from the same dance studio!</figcaption>
-                </figure>
-                
-            </div>
+            <section className="content">
+                <h3>Some of my hobbies include</h3>
+                <div className="hobby">
+                    <h4>Theatre</h4>
+                    <img className="float-left" src={theatrePoster} alt="" />
+                    <p>Ever since Highschool I've loved theatre.</p>
+                    <p>I have been able to have some of the best experiences of my life both on stage and off. I've acted, rigged lighting, created props and directed. All of which are completely different and completely rewarding.</p>
+                    <p>Some of my favourite memories are being able to play as a villian with Castlemaine Theate Company which was a long term goal and a truly dark horror with Frankensteins Children.</p>
+                    <p>Or the time I was asked to be the Technical Director for Bendigo Theatrre Company's TenX10 series of plays. Which was such a great learning experience and gave me new ways to stretch my skills in a really supportive environment.</p>
+                </div>
+                <div className="hobby">
+                    <h4>Board Games</h4>
+                    <img className="float-right boardgame-photo" src={boardGamePhoto} alt="" />
+                    <p>Ever since I was really young I played games. First Checker with my grandfather and chess with my Dad.</p>
+                    <p>When I was in my mid twenties I discovered so called "hobby" boardgames which turned first into a shelf, then a whole bookshelf, then a gaming club, and now the "shame cupboard".</p>
+                    <p>We don't speak about the shame cupboard.</p>
+                </div>
+                <div className="hobby">
+                    <h4>Dance</h4>
+                    <p>In the last 6 years I've discovered the world of Ballroom and Latin dance. Due to little things like a pandemic I haven't been able to do it for the last 2 years. I can't wait for the post COVID world where I get to go back out and move across the dancefloor!</p>
+                    <figure>
+                        <iframe className="video" width="560" height="315" src="https://www.youtube.com/embed/BFaA8Mm1syk?start=18" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                        <figcaption>Disclaimer - this is <i>not</i> me, but is from the same dance studio!</figcaption>
+                    </figure>
+                    
+                </div>
+            </section>
+            
         </div>
     )
 
--- src/Views/Final/Portfolio/Portfolio.js
+++ src/Views/Final/Portfolio/Portfolio.js
@@ -7,72 +7,75 @@ function Portfolio(props) {
 
     return (
         <div className="PortfolioFinal">
-            <div className="projects">
-                <h3>Projects I've worked on</h3>
-                <div className="project">
-                    <h4>Project 1: Tic Tac Toe</h4>
-                    <div>
-                        <a href="https://rshipard.github.io/tictactoe/" target="_blank">
-                            <img src={ticTacToeScreenshot} alt="" />
-                        </a>
-                        <br />
-                        <h4>Lessons learnt:</h4>
-                        <p>My first attempt at DOM manipulation. During which I learn how valuable event listeners are and possibly more importantly when to disable them. I am sure there are 100 ways this could have been done, but I was particularly happy with how I analysed the win conditions.</p>
-                        <p>This taught me a great deal about simply concepts that can be surprisingly hard to grasp, like the myriad ways people can, and will, click on something out of order. How you need to allow for that while not necessarily adjusting your code, but possibly adjusting HOW you allow people to interact with the page. Not letting someone interact with you page (if it's outside what you intent) is just as important as making your page interactive in the first place.</p>
-                        <h4>What I'd like to improve</h4>
-                        <ul>
-                            <li>The page looks flat and the colour scheme is a bit to dark</li>
-                            <li>When the game is over I would like to add a reset feature</li>
-                            <li>Scoring that tracks between rounds</li>
-                        </ul>
-                        <h4>Bugs:</h4>
-                        <p>You can overwrite a another players selection, and this will still increment the turn counter. I would like to go back and change these but given it's a first project I'm not likely to do so anytime soon.</p>
+            <section className="content">
+                <div className="projects">
+                    <h3>Projects I've worked on</h3>
+                    <div className="project">
+                        <h4>Project 1: Tic Tac Toe</h4>
+                        <div>
+                            <a href="https://rshipard.github.io/tictactoe/" target="_blank">
+                                <img src={ticTacToeScreenshot} alt="" />
+                            </a>
+                            <br />
+                            <h4>Lessons learnt:</h4>
+                            <p>My first attempt at DOM manipulation. During which I learn how valuable event listeners are and possibly more importantly when to disable them. I am sure there are 100 ways this could have been done, but I was particularly happy with how I analysed the win conditions.</p>
+                            <p>This taught me a great deal about simply concepts that can be surprisingly hard to grasp, like the myriad ways people can, and will, click on something out of order. How you need to allow for that while not necessarily adjusting your code, but possibly adjusting HOW you allow people to interact with the page. Not letting someone interact with you page (if it's outside what you intent) is just as important as making your page interactive in the first place.</p>
+                            <h4>What I'd like to improve</h4>
+                            <ul>
+                                <li>The page looks flat and the colour scheme is a bit to dark</li>
+                                <li>When the game is over I would like to add a reset feature</li>
+                                <li>Scoring that tracks between rounds</li>
+                            </ul>
+                            <h4>Bugs:</h4>
+                            <p>You can overwrite a another players selection, and this will still increment the turn counter. I would like to go back and change these but given it's a first project I'm not likely to do so anytime soon.</p>
+                        </div>
                     </div>
-                </div>
-                <br />
-                <div className="project">
-                    <h4>Project 2: DIY Genie</h4>
-                    <div>
-                        <a href="https://morning-island-37915.herokuapp.com/">
-                            <img src={DIYGenieScreenshot} alt="" />
-                        </a>
-                        <br />
-                        <p>I wanted to make a way for friends and family to create wishlists, stating exactly what they want, then for the rest of us to collaborate on getting them, without ruining the surprise for the one who had made the wishlist.</p>
-                        <h4>Lessons learnt:</h4>
-                        <p>Displaying information that collates information from multiple database tables and then calculating things from them is not that hard IF you have set up the tables thoughtfully.</p>
-                        <p>When trouble-shooting it is a massive advantage to constantly check exactly what it is your various functions and data queries are returning.</p>
-                        <h4>What I'd like to improve</h4>
-                        <ul>
-                            <li>The actual lists and CSS were rushed, and they need work to improve the user experience.</li>
-                            <li>The names for the various categories need improvement and honestly I'd like to go back and rework the whole database with the benefit of hindsight.</li>
-                            <li>I wanted to add a lit more categories like "size", "colour" and the ability for people to upload images and links to exactly what they wanted.</li>
-                        </ul>
-                        <h4>Bugs:</h4>
-                        <p>None that I am aware of the but core functions are very bare bones.</p>
+                    <br />
+                    <div className="project">
+                        <h4>Project 2: DIY Genie</h4>
+                        <div>
+                            <a href="https://morning-island-37915.herokuapp.com/">
+                                <img src={DIYGenieScreenshot} alt="" />
+                            </a>
+                            <br />
+                            <p>I wanted to make a way for friends and family to create wishlists, stating exactly what they want, then for the rest of us to collaborate on getting them, without ruining the surprise for the one who had made the wishlist.</p>
+                            <h4>Lessons learnt:</h4>
+                            <p>Displaying information that collates information from multiple database tables and then calculating things from them is not that hard IF you have set up the tables thoughtfully.</p>
+                            <p>When trouble-shooting it is a massive advantage to constantly check exactly what it is your various functions and data queries are returning.</p>
+                            <h4>What I'd like to improve</h4>
+                            <ul>
+                                <li>The actual lists and CSS were rushed, and they need work to improve the user experience.</li>
+                                <li>The names for the various categories need improvement and honestly I'd like to go back and rework the whole database with the benefit of hindsight.</li>
+                                <li>I wanted to add a lit more categories like "size", "colour" and the ability for people to upload images and links to exactly what they wanted.</li>
+                            </ul>
+                            <h4>Bugs:</h4>
+                            <p>None that I am aware of the but core functions are very bare bones.</p>
+                        </div>
                     </div>
-                </div>
-                <br />
-                <div className="project">
-                    <h4>Project 3: QuizWatch</h4>
-                    <div>
-                        <a href="https://afternoon-sands-32182.herokuapp.com/" target="_blank">
-                            <img src={quizwatchScreenshot} alt="" />
-                        </a>
-                        <br />
-                        <p>This was a group project to create a flashcard program to aid in learning new information. Given that we had not seen or used React prior it turns out we had created a servicable clone on our own!</p>
-                        <h4>Lessons learnt:</h4>
-                        <p>When you're trying to add multiple features the first thing you need to do as a group is clearly communicate and break the wrok apart. That said we got pretty good at resolving git merge conflicts, and then creating systems so they happened less and less.</p>
-                        <p>Managing a large team project is a job unto itself and apparently I love refactoring code. Something I hadn't fully realised until now.</p>
-                        <h4>What I'd like to improve</h4>
-                        <ul>
-                            <li>The site itself prbably needs a more interactive and detailed walkthrough of it's functions</li>
-                            <li>I'd love to create an e-mail notification system you can customies to how many flashcards you have due, or how long they've been "waiting" for you</li>
-                            <li>The "intro" flashcards should be more generic in their questions and hints, to aid in the user easily understanding the concepts of the program.</li>
-                        </ul>
-                    </div>
+                    <br />
+                    <div className="project">
+                        <h4>Project 3: QuizWatch</h4>
+                        <div>
+                            <a href="https://afternoon-sands-32182.herokuapp.com/" target="_blank">
+                                <img src={quizwatchScreenshot} alt="" />
+                            </a>
+                            <br />
+                            <p>This was a group project to create a flashcard program to aid in learning new information. Given that we had not seen or used React prior it turns out we had created a servicable clone on our own!</p>
+                            <h4>Lessons learnt:</h4>
+                            <p>When you're trying to add multiple features the first thing you need to do as a group is clearly communicate and break the wrok apart. That said we got pretty good at resolving git merge conflicts, and then creating systems so they happened less and less.</p>
+                            <p>Managing a large team project is a job unto itself and apparently I love refactoring code. Something I hadn't fully realised until now.</p>
+                            <h4>What I'd like to improve</h4>
+                            <ul>
+                                <li>The site itself prbably needs a more interactive and detailed walkthrough of it's functions</li>
+                                <li>I'd love to create an e-mail notification system you can customies to how many flashcards you have due, or how long they've been "waiting" for you</li>
+                                <li>The "intro" flashcards should be more generic in their questions and hints, to aid in the user easily understanding the concepts of the program.</li>
+                            </ul>
+                        </div>
 
+                    </div>
                 </div>
-            </div>
+            </section>
+            
         </div>
     )
 
